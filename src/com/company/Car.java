2f8755bcb06d72cbc2ac7da66d80d5071a69e14b
tduyen.86@gmail.com <tduyen.86@gmail.com>
2021-02-19 13:56:29 +0700

    finished

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -12,4 +12,19 @@ public class Car {
         numberOfCars++;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
 }
